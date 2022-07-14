3e98effe6b5bc67c8e9dacd682bfae822e214872
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2022-07-14 10:16:43 -0300

    14/7

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,80 @@
-$("li.seletor").click((e)=>{
-    $(".sub-list").removeClass("active")
-    let element = e.target.children[0]
-    element.classList.add("active")
-})
+let subList = $("nav>ul>li")
+let select = ""
+events()
+update()
 
-$(window).click((e)=>{
-    if(e.target.classList[0] != "seletor"){
-        $(".sub-list").removeClass("active")
+
+
+function update() {
+    var largura = window.innerWidth;
+    $("main>div").hide()
+    id = $(".select").attr("id")
+    $("." + id).show()
+    if (largura < 800) {
+
+
+
+
+
+        $("nav").html(`
+        <ul>
+            <li class="select" id="${id}">${select}</li>
+            <li class="seletor" id="more">
+            Mais
+            <ul class="sub-list more">
+            <li>
+            </li>
+            </ul>`)
+
+        $(".sub-list").html(subList)
+
+        $("ul").css({
+            "grid-template-columns": "auto auto",
+            "text-align": "center"
+        })
+
+        $("nav>ul>li.select").css({
+            "border-right": "1px solid black"
+        })
+
+        $(".sub-list").css({
+            "width": "100%"
+        })
+
+        $(".itens").css({
+            "background-color": "gray",
+            "z-index": "5"
+        })
+
+        $("main").css({
+            "width": "95%",
+            "margin": "auto",
+            "margin-top": "10px"
+        })
+
+        events()
+
     }
-})
+}
+
+function events() {
+    $(".seletor").click((e) => {
+        if (e.target.id != "") {
+            $("." + e.target.id).addClass("active")
+        }
+    })
+
+    $(window).click((e) => {
+        if (e.target.classList[0] != "seletor") {
+            $(".sub-list").removeClass("active")
+        }
+    })
+
+    $("li").not(".seletor").click((e) => {
+        $("li.select").removeClass("select")
+        e.target.classList.add("select")
+
+        update()
+    })
+    select = $(".select").html()
+}
